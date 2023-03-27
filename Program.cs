b0128a7031181aea73d97317c754f53220907819
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-27 02:19:56 -1200

    Inserting 6,30,70 in linked list

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void  Main(String[] args)
         {
             Console.WriteLine("Welcom to the Linked List Program");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
         }
     }
 }
