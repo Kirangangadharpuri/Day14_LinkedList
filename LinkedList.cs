32720ae47e48979877f3c691ffb8a01314435933
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-27 15:47:14 -1200

    Code for insert data at any position linked list

---
+++ LinkedList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedListAssignment
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Linked List", node.data);
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal Node InsertAtPerticlarPosition(int position,int Data) 
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position==1)
+            {
+                var newNode=new Node(Data);
+                newNode.next = this.head;
+                head= newNode;
+            }
+            else
+            {
+                while(position-- !=0)
+                {
+                    if(position==1)
+                    {
+                        Node node=new Node(Data);
+                        node.next=this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
+    }
+}
