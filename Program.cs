917a3e53363a6db178da17d631b0a6c6915cc9b6
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-27 19:36:02 -1200

    initialize values to node in linkde list

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace LinkedListAssignment
+﻿using System.Collections.Generic;
+
+namespace LinkedListAssignment
 {
     class Program
     {
         public static void  Main(String[] args)
         {
             Console.WriteLine("Welcom to the Linked List Program");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+            Console.WriteLine("LinkedList Sequence: 56->30->70");
+            linkedList.RemoveLastNode();
+            linkedList.Display();
+            Console.WriteLine("Final Sequence: 30->70");
         }
     }
 }
