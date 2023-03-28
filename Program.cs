ecdfebaf6180b34a0fd0ec008ff1115ac0dd4ea0
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-27 15:46:43 -1200

    Inserting 30 between 56 & 70

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace LinkedListAssignment
+﻿using System.Collections.Generic;
+
+namespace LinkedListAssignment
 {
     class Program
     {
         public static void  Main(String[] args)
         {
-            Console.WriteLine("Welcom to the Linked List Program");
+            Console.WriteLine("Welcome to the Linked List Program");
+
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(70);
+            linkedList.Display();
+            linkedList.InsertAtPerticlarPosition(2,30);
+            Console.WriteLine("***************************");
+
+            Console.WriteLine("30 inserted between 56 and 70");
+            linkedList.Display();
+
+
         }
     }
 }
