64df0399c0c41ea77b98a829e5fb198ee2383d47
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-27 19:45:48 -1200

    initialize values to node in linkde list

--- Program.cs
+++ Program.cs
@@ -5,6 +5,18 @@
         public static void  Main(String[] args)
         {
             Console.WriteLine("Welcom to the Linked List Program");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            Console.WriteLine("Node with data 56 is first Created");
+
+            linkedList.Add(30);
+            Console.WriteLine("Next Append 30 to 56");
+
+            linkedList.Add(70);
+            Console.WriteLine("Finally Append 70 to 30");
+
+            linkedList.Display();
+            Console.WriteLine("LinkedList Sequence: 56->30->70");
         }
     }
 }
