4b7d05527f3915b29666e09300e847473d75e8e5
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-29 03:10:08 -1200

    inserting the values in linked list

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         public static void  Main(String[] args)
         {
             Console.WriteLine("Welcom to the Linked List Program");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+            linkedList.Search(30);
         }
     }
 }
