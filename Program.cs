216a8ad22ad6a4aed8115225bbe537fe38c24915
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-29 03:35:19 -1200

    inserting the values in linked list and aad new node

--- Program.cs
+++ Program.cs
@@ -5,6 +5,18 @@
         public static void  Main(String[] args)
         {
             Console.WriteLine("Welcom to the Linked List Program");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+            linkedList.Search(30);
+            linkedList.InsertNode(3, 40);
+            Console.WriteLine("***************************");
+
+            linkedList.Display();
+            Console.WriteLine("40 inserted after 30");
+
         }
     }
 }
